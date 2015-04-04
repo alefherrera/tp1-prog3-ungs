31c316f6937f2ddd7369ae2c180ea1c2e4f4b055
andiblas <andy_blas@hotmail.com>
2015-04-04 13:15:30 -0300

    ADD equals y hashCode a clase palabra para usar en HashSet de servicio de palabras

--- tp1-prog3/src/models/Palabra.java
+++ tp1-prog3/src/models/Palabra.java
@@ -4,6 +4,31 @@ public abstract class Palabra {
 
 	protected String texto;
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((texto == null) ? 0 : texto.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Palabra other = (Palabra) obj;
+		if (texto == null) {
+			if (other.texto != null)
+				return false;
+		} else if (!texto.equals(other.texto))
+			return false;
+		return true;
+	}
+
 	public String getTexto() {
 		return texto;
 	}
